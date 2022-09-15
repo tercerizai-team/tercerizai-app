9414503f2a2abd97a397624544d0f97989fc3f08
PedroSoprano <vonhishi@gmail.com>
2022-09-14 21:05:55 -0400

    fix: adicionando userId ao local storage

--- src/pages/signIn/index.jsx
+++ src/pages/signIn/index.jsx
@@ -34,6 +34,7 @@ function SignIn() {
       .post(`${baseUrl}/login`, data)
       .then((res) => {
         localStorage.setItem("token", res.data.token);
+        localStorage.setItem("userId", res.data.userId);
         if (res.data.isProvider) {
           navigate("/dashboard");
         } else {
